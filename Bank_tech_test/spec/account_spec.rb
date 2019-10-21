898e7fcdc0736258ae9ab86e17c01d34370dc783
Julian George <jules104@gmail.com>
2019-10-21 11:50:01 +0100

    Test - prints out an empty bank statement

---
+++ Bank_tech_test/spec/account_spec.rb
@@ -0,0 +1,8 @@
+require './account'
+
+describe 'statement' do
+  it 'prints out an empty bank statement' do
+    account = Account.new
+    expect(account.statement).to eq"(date       || credit  || debit  || balance)"
+  end
+end
