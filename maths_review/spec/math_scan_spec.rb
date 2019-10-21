3b0ed981b5b0a3abf893c52fa1bce341aef678bb
Julian George <jules104@gmail.com>
2019-10-21 19:40:19 +0100

    scan_paper result so result not hard coded

--- maths_review/spec/math_scan_spec.rb
+++ maths_review/spec/math_scan_spec.rb
@@ -4,4 +4,8 @@ describe 'scan paper ' do
   it 'Input is 1 + 1, returns ["1 + 1", 2]' do
     expect(scan_paper("1 + 1")).to eq(["1 + 1", 2])
   end
+
+  it 'Input is 1 * 1 returns ["1 + 1", 1]' do
+    expect(scan_paper("1 * 1")).to eq(["1 * 1", 1])
+  end
 end
