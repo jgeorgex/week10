bf5788a20bdf774a44959761017261538f14cf22
Julian George <jules104@gmail.com>
2019-10-21 14:48:09 +0100

    Add deposit method to log credits made to account

--- Bank_tech_test/account.rb
+++ Bank_tech_test/account.rb
@@ -1,9 +1,18 @@
 class Account
 
+  def initialize
+  @credit = []
+  end
+
   def statement
     puts "date || credit || debit || balance"
     puts "14/01/2012 || || 500.00 || 2500.00"
     puts "13/01/2012 || 2000.00 || || 3000.00"
     puts "10/01/2012 || 1000.00 || || 1000.00"
   end
+
+  def deposit(deposit_value)
+      @credit << deposit_value
+  end
+
 end
--- Bank_tech_test/spec/account_spec.rb
+++ Bank_tech_test/spec/account_spec.rb
@@ -4,9 +4,17 @@ describe 'statement' do
   it 'prints out bank statement' do
     account = Account.new
     expect(account.statement).to eq puts "date || credit || debit || balance"
-                                    puts "14/01/2012 || || 500.00 || 2500.00"
-                                    puts "13/01/2012 || 2000.00 || || 3000.00"
-                                    puts "10/01/2012 || 1000.00 || || 1000.00"
+    puts "14/01/2012 || || 500.00 || 2500.00"
+    puts "13/01/2012 || 2000.00 || || 3000.00"
+    puts "10/01/2012 || 1000.00 || || 1000.00"
+  end
+
+  describe 'credit' do
+    it 'credits account with 1000.00' do
+      account = Account.new
+      @credit = []
+      expect(account.deposit(1000.00)).to eq(@credit = [1000.00])
+    end
   end
 
 end
