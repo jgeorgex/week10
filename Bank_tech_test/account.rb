de807ae85dd1a9395848c54165676f32868e766c
Julian George <jules104@gmail.com>
2019-10-21 15:42:12 +0100

    Restart statement test to have only statement headings

--- Bank_tech_test/account.rb
+++ Bank_tech_test/account.rb
@@ -6,10 +6,7 @@ class Account
   end
 
   def statement
-    puts "date || credit || debit || balance"
-    puts "14/01/2012 || || 500.00 || 2500.00"
-    puts "13/01/2012 || 2000.00 || || 3000.00"
-    puts "10/01/2012 || 1000.00 || || 1000.00"
+    "date || credit || debit || balance"
   end
 
   def deposit(deposit_value)
--- Bank_tech_test/spec/account_spec.rb
+++ Bank_tech_test/spec/account_spec.rb
@@ -3,10 +3,7 @@ require './account'
 describe 'statement' do
   it 'prints out bank statement' do
     account = Account.new
-    expect(account.statement).to eq puts "date || credit || debit || balance"
-    puts "14/01/2012 || || 500.00 || 2500.00"
-    puts "13/01/2012 || 2000.00 || || 3000.00"
-    puts "10/01/2012 || 1000.00 || || 1000.00"
+    expect(account.statement).to eq("date || credit || debit || balance")
   end
 
   describe 'credit' do
