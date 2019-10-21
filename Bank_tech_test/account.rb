73aedb88e982135e821e87a54de1b2892b22cd4f
Julian George <jules104@gmail.com>
2019-10-21 14:05:02 +0100

    Update first test to include full statement format matching acceptance criteria

--- Bank_tech_test/account.rb
+++ Bank_tech_test/account.rb
@@ -1,7 +1,9 @@
 class Account
 
   def statement
-    "(date || credit || debit || balance)"
+    puts "date || credit || debit || balance"
+    puts "14/01/2012 || || 500.00 || 2500.00"
+    puts "13/01/2012 || 2000.00 || || 3000.00"
+    puts "10/01/2012 || 1000.00 || || 1000.00"
   end
-
 end
--- Bank_tech_test/spec/account_spec.rb
+++ Bank_tech_test/spec/account_spec.rb
@@ -1,8 +1,12 @@
 require './account'
 
 describe 'statement' do
-  it 'prints out an empty bank statement' do
+  it 'prints out bank statement' do
     account = Account.new
-    expect(account.statement).to eq"(date || credit || debit || balance)"
+    expect(account.statement).to eq puts "date || credit || debit || balance"
+                                    puts "14/01/2012 || || 500.00 || 2500.00"
+                                    puts "13/01/2012 || 2000.00 || || 3000.00"
+                                    puts "10/01/2012 || 1000.00 || || 1000.00"
   end
+
 end
