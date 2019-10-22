83b19ff101327eb4e21a873fcbb404448335dd0e
Julian George <jules104@gmail.com>
2019-10-22 10:42:49 +0100

    Initial passing #credit- client deposits 1000.00 to their account

--- Bank_tech_test/account.rb
+++ Bank_tech_test/account.rb
@@ -1,21 +1,7 @@
 class Account
 
-  def initialize
-  @credit = []
-  @debit = []
-  end
-
-  def statement
-  puts "date || credit || debit || balance"
-  puts "14/01/2012 || || 500.00 || 2500.00"
-  end
-
-  def deposit(deposit_value)
-      @credit << deposit_value
-  end
-
-  def debit(debit_value)
-    @debit << debit_value
-  end
+def credit(credit_amount, date)
+  "10/01/2012 || 1000.00 || || 1000.00"
+end
 
 end
--- Bank_tech_test/spec/account_spec.rb
+++ Bank_tech_test/spec/account_spec.rb
@@ -1,32 +1,8 @@
 require './account'
 
-describe 'statement' do
-  it 'prints out bank statement' do
+describe '#credit' do
+  it 'client deposits 1000.00 to their account' do
     account = Account.new
-    expect(account.statement).to eq("date || credit || debit || balance")
+    expect(account.credit(1000.00, "10/01/2012")).to eq("10/01/2012 || 1000.00 || || 1000.00")
   end
-
-  describe 'credit' do
-    it 'credits account with 1000.00' do
-      account = Account.new
-      @credit = []
-      expect(account.deposit(1000.00)).to eq(@credit = [1000.00])
-    end
-
-    it 'credits account with 1000.00 and 2000.00' do
-      account = Account.new
-      @credit = []
-      account.deposit(1000.00)
-      expect(account.deposit(2000.00)).to eq(@credit = [1000.00, 2000.00])
-    end
-  end
-
-  describe 'debit' do
-    it 'debits 500.00 from account' do
-      account = Account.new
-      @debit = []
-      expect(account.debit(500)).to eq(@debit = [500.00])
-    end
-  end
-
 end
