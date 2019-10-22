0201527c0d93425824a925192c511c6c2e22e648
Julian George <jules104@gmail.com>
2019-10-22 18:21:58 +0100

    Refactored for leap years divisible by 400

--- leap_years/is_year_a_leap_year.rb
+++ leap_years/is_year_a_leap_year.rb
@@ -1,3 +1,7 @@
 def is_year_a_leap_year(year)
-  "#{year} is NOT a leap year"
+  if year % 400 == 0
+    "#{year} IS a leap year"
+  else
+    "#{year} is NOT a leap year"
+  end
 end
--- leap_years/spec/is_year_a_leap_year_spec.rb
+++ leap_years/spec/is_year_a_leap_year_spec.rb
@@ -3,6 +3,23 @@ require './is_year_a_leap_year'
 describe '#is_year_a_leap_year' do
   it '2017 is not a leap year' do
     expect(is_year_a_leap_year(2017)).to eq("2017 is NOT a leap year")
-
   end
+
+  it '2018 is not a leap year' do
+    expect(is_year_a_leap_year(2018)).to eq("2018 is NOT a leap year")
+  end
+
+  it '400 is a leap year' do
+    expect(is_year_a_leap_year(400)).to eq("400 IS a leap year")
+  end
+
+  it '2000 is a leap year' do
+    expect(is_year_a_leap_year(2000)).to eq("2000 IS a leap year")
+  end
+
+  it '1700 is not a leap year' do
+    expect(is_year_a_leap_year(1700)).to eq("1700 is NOT a leap year")
+  end
+
+
 end
