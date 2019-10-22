d63ab6b5a5db35d2743218b8fda30807aea39475
Julian George <jules104@gmail.com>
2019-10-22 14:54:41 +0100

    Add print statement method

--- Bank_tech_test/account.rb
+++ Bank_tech_test/account.rb
@@ -4,11 +4,14 @@ def initialize
 @balance = 0
 @deposits = []
 @withdrawals = []
+@statement = []
 end
 
 def credit(date, credit_amount)
   @deposits << credit_amount
-  "#{date}|| #{credit_amount}0 || || #{update_balance}0"
+  credit_transaction = "#{date}|| #{credit_amount}0 || || #{update_balance}0"
+  @statement << credit_transaction
+  return credit_transaction
 end
 
 def update_balance
@@ -17,11 +20,17 @@ end
 
 def debit(date, debit_amount)
   @withdrawals << debit_amount
-  "#{date}|| || #{debit_amount}0 || #{update_balance}0"
+  debit_transaction = "#{date}|| || #{debit_amount}0 || #{update_balance}0"
+  @statement << debit_transaction
+  return debit_transaction
 end
 
 def print_statement
-  "date || credit || debit || balance"
+  puts "date || credit || debit || balance"
+  statement =  @statement
+  statement.each do |transaction|
+    puts transaction
+  end
 end
 
 end
