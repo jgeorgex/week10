eb7404e74ab982d4fd6118977470fd0d9fae3cba
Julian George <jules104@gmail.com>
2019-10-23 10:44:38 +0100

    Refactor to remove need for manual date entry argument

--- Bank_tech_test/account.rb
+++ Bank_tech_test/account.rb
@@ -1,5 +1,7 @@
 class Account
 
+  attr_accessor :date
+
   def initialize
     @balance = 0
     @deposits = []
@@ -7,11 +9,11 @@ class Account
     @statement = ["date || credit || debit || balance"]
   end
 
-  def credit(date, credit_amount)
+  def credit(credit_amount)
     @deposits << credit_amount
-    time = Time.new
-    time = time.strftime("%d/%m/%Y")
-    credit_transaction = "#{time} || #{credit_amount}0 || || #{update_balance}0"
+    date = Time.new
+    date = date.strftime("%d/%m/%Y")
+    credit_transaction = "#{date} || #{credit_amount}0 || || #{update_balance}0"
     @statement << credit_transaction
     return credit_transaction
   end
@@ -20,11 +22,11 @@ class Account
     @balance = @deposits.sum - @withdrawals.sum
   end
 
-  def debit(date, debit_amount)
+  def debit(debit_amount)
     @withdrawals << debit_amount
-    time = Time.new
-    time = time.strftime("%d/%m/%Y")
-    debit_transaction = "#{time} || || #{debit_amount}0 || #{update_balance}0"
+    date = Time.new
+    date = date.strftime("%d/%m/%Y")
+    debit_transaction = "#{date} || || #{debit_amount}0 || #{update_balance}0"
     @statement << debit_transaction
     return debit_transaction
   end
@@ -34,4 +36,5 @@ class Account
       puts transaction
     end
   end
+
 end
--- Bank_tech_test/spec/account_spec.rb
+++ Bank_tech_test/spec/account_spec.rb
@@ -3,22 +3,22 @@ require './account'
 describe '#credit' do
   it 'client deposits 1000.00 to their account' do
     account = Account.new
-    expect(account.credit("10/01/2012 ", 1000.00)).to eq("23/10/2019 || 1000.00 || || 1000.00")
+    expect(account.credit(1000.00)).to eq("23/10/2019 || 1000.00 || || 1000.00")
   end
 
   it 'client deposits 1000.00 then 2000.00 to their account' do
     account = Account.new
-    account.credit("23/10/2019 ", 1000.00)
-    expect(account.credit("23/10/2019 ", 2000.00)).to eq("23/10/2019 || 2000.00 || || 3000.00")
+    account.credit(1000.00)
+    expect(account.credit(2000.00)).to eq("23/10/2019 || 2000.00 || || 3000.00")
   end
 end
 
 describe '#debit' do
   it 'client withdraws 500.00 from their account' do
     account = Account.new
-    account.credit("23/10/2019 ", 1000.00)
-    account.credit("23/10/2019 ", 2000.00)
-    expect(account.debit("23/10/2019 ", 500.00)).to eq("23/10/2019 || || 500.00 || 2500.00")
+    account.credit(1000.00)
+    account.credit(2000.00)
+    expect(account.debit(500.00)).to eq("23/10/2019 || || 500.00 || 2500.00")
   end
 end
 
@@ -30,7 +30,7 @@ describe '#print_statement' do
 
   it 'prints out a statement after one transaction' do
     account = Account.new
-    account.credit("10/01/2012 ", 1000.00)
+    account.credit(1000.00)
     expect(account.print_statement).to eq("date || credit || debit || balance, 10/01/2012 || 1000.00 || || 1000.00")
   end
 end
