3e8e6e00fe8954864baae76c4558ab5a0fff6b12
Julian George <jules104@gmail.com>
2019-10-24 17:20:22 +0100

    Refactor tests to remove unecessary decsribes

--- scrabble/spec/scrabble_spec.rb
+++ scrabble/spec/scrabble_spec.rb
@@ -1,22 +1,20 @@
 require './scrabble'
 
 describe '#score' do
-  scrabble = Scrabble.new
+
   it 'score is 1 if word is "A" ' do
+    scrabble = Scrabble.new
     expect(scrabble.score('A')).to eq(1)
   end
 
-  describe '#score' do
+  it 'score is 4 if word is "F" ' do
     scrabble = Scrabble.new
-    it 'score is 4 if word is "F" ' do
-      expect(scrabble.score('F')).to eq(4)
-    end
+    expect(scrabble.score('F')).to eq(4)
   end
 
-  describe '#score' do
+
+  it 'score is 5 if word is "K" ' do
     scrabble = Scrabble.new
-    it 'score is 5 if word is "K" ' do
-      expect(scrabble.score('K')).to eq(5)
-    end
+    expect(scrabble.score('K')).to eq(5)
   end
 end
