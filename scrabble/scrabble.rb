c4a500630857c018aa44a4cc568b26de384b860e
Julian George <jules104@gmail.com>
2019-10-26 11:44:43 +0100

    Score method returns a score of 6 when the word is adbfkjq

--- scrabble/scrabble.rb
+++ scrabble/scrabble.rb
@@ -7,15 +7,31 @@ class Scrabble
   def score
     word_score = []
     @word.each_char { |letter|
-    if letter == 'A'
+    if letter == 'a'
       word_score << 1
     elsif
-      letter == 'F'
+      letter == 'd'
+      word_score << 2
+    elsif
+      letter == 'b'
+      word_score << 3
+    elsif
+      letter == 'f'
       word_score << 4
+    elsif
+      letter == 'k'
+      word_score << 5
+    elsif
+      letter == 'j'
+      word_score << 8
+    elsif
+      letter == 'q'
+      word_score << 10
     else
       word_score << 0
     end
     }
     word_score.sum
   end
+
 end
--- scrabble/spec/scrabble_spec.rb
+++ scrabble/spec/scrabble_spec.rb
@@ -7,18 +7,24 @@ describe '#score' do
     expect(scrabble.score).to eq(0)
   end
 
-  it 'returns a score of 1 when the word is "A"' do
-    scrabble = Scrabble.new('A')
+  it 'returns a score of 1 when the word is "a"' do
+    scrabble = Scrabble.new('a')
     expect(scrabble.score).to eq(1)
   end
 
-  it 'returns a score of 4 when the word is "F"' do
-    scrabble = Scrabble.new('F')
+  it 'returns a score of 4 when the word is "f"' do
+    scrabble = Scrabble.new('f')
     expect(scrabble.score).to eq(4)
   end
 
-  it 'returns a score of 5 when the word is "AF"' do
-    scrabble = Scrabble.new('AF')
+  it 'returns a score of 5 when the word is "af"' do
+    scrabble = Scrabble.new('af')
     expect(scrabble.score).to eq(5)
   end
+
+  it 'returns a score of 33 when the word is "adbfkjq"' do
+    scrabble = Scrabble.new('adbfkjq')
+    expect(scrabble.score).to eq(33)
+  end
+
 end
